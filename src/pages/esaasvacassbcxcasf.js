724828aa2a071b62db040ad120245ea562cdc475
Hanifan Azka <hanifan.azka@gmail.com>
2021-03-21 18:53:40 +0700

    Primitive Viewer

---
+++ src/pages/esaasvacassbcxcasf.js
@@ -0,0 +1,55 @@
+import * as React from "react"
+import PropTypes from 'prop-types';
+import { Formik, Form, Field } from 'formik';
+import Select from "react-select";
+import Amplify, { Auth, API } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import awsconfig from '../data/aws-exports'
+import SEO from "../components/seo"
+import { groupedOptions } from "../data/santri"
+import signUp from "../functions/signup"
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+Amplify.configure(awsconfig);
+
+
+async function postData(data) {
+    const apiName = 'api66db2874';
+    const path = '/coblos';
+    const myInit = { // OPTIONAL
+        body: data, // replace this with attributes you need
+        headers: {}, // OPTIONAL
+    };
+
+    return await API.post(apiName, path, myInit);
+}
+
+
+
+class Hasil extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { data: "" }
+    }
+
+    componentDidMount() {
+        API
+            .get("api66db2874", "/coblos")
+            .then(response => {
+                this.setState({ data: response })
+            })
+            .catch(error => {
+                console.log(error.response);
+            });
+    }
+    render() {
+    return (
+        <>
+        {this.state.data}
+        </>
+    )
+}
+}
+
+export default Hasil
